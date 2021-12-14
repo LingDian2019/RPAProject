0f9f981108836675d123e7da88d492157a7f35a0
LingDian <857388137@qq.com>
2021-12-14 16:55:32 +0800

    保存

---
+++ src/RPAProjectTool/ConsoleLogServer.cs
@@ -0,0 +1,47 @@
+﻿using System.Drawing;
+using Console = Colorful.Console;
+
+namespace RPAProjectTool
+{
+    /// <summary>
+    /// 控制台日志服务
+    /// </summary>
+    public static class ConsoleLogServer
+    {
+        /// <summary>
+        /// 写日志
+        /// </summary>
+        /// <param name="message">消息</param>
+        public static void DefaultLog(string message)
+        {
+            Console.WriteLine(message, Color.White);
+        }
+
+        /// <summary>
+        /// 写日志
+        /// </summary>
+        /// <param name="message">消息</param>
+        public static void WarnLog(string message)
+        {
+            Console.WriteLine(message, Color.Yellow);
+        }
+
+        /// <summary>
+        /// 写日志
+        /// </summary>
+        /// <param name="message">消息</param>
+        public static void ErrorLog(string message)
+        {
+            Console.WriteLine(message, Color.Red);
+        }
+
+        /// <summary>
+        /// 写日志
+        /// </summary>
+        /// <param name="message">消息</param>
+        public static void SucceedLog(string message)
+        {
+            Console.WriteLine(message, Color.Green);
+        }
+    }
+}
--- src/RPAScreenshot/Program.cs
+++ src/RPAScreenshot/Program.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 using System.Threading.Tasks;
 using Microsoft.Playwright;
+using RPAProjectTool;
 
-namespace KJToTTXSupplierImport
+namespace RPAScreenshot
 {
     internal class Program
     {
         public static async Task Main()
         {
+            ConsoleLogServer.DefaultLog("打开网页，并截图保存");
+
             using var playwright = await Playwright.CreateAsync();
             await using var browser = await playwright.Firefox.LaunchAsync(new BrowserTypeLaunchOptions
             {
@@ -16,8 +19,12 @@ namespace KJToTTXSupplierImport
             });
             var page = await browser.NewPageAsync();
             await page.GotoAsync("https://playwright.dev/dotnet");
+
             //截图
             await page.ScreenshotAsync(new PageScreenshotOptions { Path = "screenshot.png" });
+
+            ConsoleLogServer.SucceedLog("截图完成");
+
             Console.ReadLine();
         }
     }
