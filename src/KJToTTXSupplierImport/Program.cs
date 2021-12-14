2939e5793a5aad5907547c684803135c14a52356
LingDian <857388137@qq.com>
2021-12-14 16:34:43 +0800

    保存

---
+++ src/KJToTTXSupplierImport/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Playwright;
+
+namespace KJToTTXSupplierImport
+{
+    internal class Program
+    {
+        public static async Task Main()
+        {
+            using var playwright = await Playwright.CreateAsync();
+            await using var browser = await playwright.Chromium.LaunchAsync();
+            var page = await browser.NewPageAsync();
+            await page.GotoAsync("https://playwright.dev/dotnet");
+            //截图
+            await page.ScreenshotAsync(new PageScreenshotOptions { Path = "screenshot.png" });
+            Console.ReadLine();
+        }
+    }
+}
