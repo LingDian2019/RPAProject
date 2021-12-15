46b845f7f61d85e5d2c1a6895b9ebc8ba48d6ff5
LingDian <857388137@qq.com>
2021-12-15 16:47:50 +0800

    保存

---
+++ src/RPADownload/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Playwright;
+using RPAProjectTool;
+
+
+namespace RPADownload
+{
+    internal class Program
+    {
+        public static async Task Main()
+        {
+            using var playwright = await Playwright.CreateAsync();
+            await using var browser = await playwright.Firefox.LaunchAsync(new BrowserTypeLaunchOptions
+            {
+                Headless = false,
+                SlowMo = 50,
+            });
+
+            ConsoleLogServer.DefaultLog("下载文件");
+            var page = await browser.NewPageAsync();
+            await page.GotoAsync("https://dotnet.microsoft.com/en-us/download");
+            // Start the task of waiting for the download
+            //var waitForDownloadTask = page.WaitForDownloadAsync();
+            // Perform the action that initiates download
+            //
+            var responseResults = await page.TextContentAsync("css=[data-bi-name='Download']");
+            await page.ClickAsync("css=[data-bi-name='Download']");
+            // Wait for the download process to complete
+            //var download = await waitForDownloadTask;
+            //var path = await download.PathAsync();
+
+            ConsoleLogServer.SucceedLog("下载完成");
+
+            Console.ReadLine();
+        }
+    }
+}
